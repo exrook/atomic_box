8b419cf0f73deff29593f98e289508b73a0f2b0d
Jacob Hughes <j@distanthills.org>
2017-04-16 13:51:23 -0400

    Initial implementation

---
+++ src/lib.rs
@@ -0,0 +1,53 @@
+use std::sync::Arc;
+use std::sync::atomic::{AtomicPtr,Ordering};
+use std::fmt;
+
+/// A struct which allows multiple threads to safely update a shared non-nullable pointer to heap data
+pub struct AtomicBox<T> {
+    ptr: AtomicPtr<Arc<T>>
+}
+
+impl<T> AtomicBox<T> {
+    /// Create a new atomic box from `data`
+    pub fn new(data: T) -> AtomicBox<T> {
+        let arc = Box::new(Arc::new(data));
+        let ptr = AtomicPtr::new(Box::into_raw(arc));
+        return AtomicBox{
+            ptr: ptr,
+        }
+    }
+    /// Get an `Arc<T>` pointing to the value currently stored in the box
+    pub fn load(&self) -> Arc<T> {
+        let arc = unsafe { &mut *self.ptr.load(Ordering::Acquire) };
+        return arc.clone();
+    }
+    /// Update the value stored in the box to point to `data`, the previous data will be reclaimed
+    /// once the last reader has dropped their reference.
+    pub fn store(&self, data: T) {
+        let arc = Box::new(Arc::new(data));
+        let ptr = Box::into_raw(arc);
+        let old_ptr = self.ptr.swap(ptr, Ordering::AcqRel);
+        unsafe {Box::from_raw(old_ptr)};
+    }
+    /// Swap the current value with `data`, returning a `boxed` `Arc<T>` pointing to the old value
+    pub fn swap(&self, data: T) -> Box<Arc<T>> {
+        let arc = Box::new(Arc::new(data));
+        let ptr = Box::into_raw(arc);
+        let old_ptr = self.ptr.swap(ptr, Ordering::AcqRel);
+        return unsafe {Box::from_raw(old_ptr)};
+    }
+}
+
+impl<T> Drop for AtomicBox<T> {
+    fn drop(&mut self) {
+        unsafe { Box::from_raw(self.ptr.load(Ordering::Acquire)) };
+    }
+}
+
+impl<T> fmt::Debug for AtomicBox<T> where T: fmt::Debug {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("AtomicBox")
+            .field("ptr", &self.load())
+            .finish()
+    }
+}
