d2245a11d78bd973ea5a8c666f16afb00c411f77
Jacob Hughes <j@distanthills.org>
2017-07-07 00:58:08 -0400

    Added some more info about the project, ran rustfmt, added license, added badges

---
+++ README.md
@@ -0,0 +1,4 @@
+Atomic Box [![Crates.io](https://img.shields.io/crates/v/atomic_box.svg?style=flat-square)](https://crates.io/crates/atomic_box) [![Crates.io](https://img.shields.io/crates/l/atomic_box.svg?style=flat-square)](https://crates.io/crates/atomic_box) [![Docs.rs](https://docs.rs/atomic_box/badge.svg)](https://docs.rs/atomic_box/)
+==========
+A struct which allows multiple threads to safely update and read from a shared non-nullable pointer to heap data
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,18 @@
+// Copyright 2017 Jacob Hughes
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 use std::sync::Arc;
-use std::sync::atomic::{AtomicPtr,Ordering};
+use std::sync::atomic::{AtomicPtr, Ordering};
 use std::fmt;
 
-/// A struct which allows multiple threads to safely update a shared non-nullable pointer to heap data
+/// A struct which allows multiple threads to safely update and read from a shared non-nullable pointer to heap data
 pub struct AtomicBox<T> {
-    ptr: AtomicPtr<Arc<T>>
+    ptr: AtomicPtr<Arc<T>>,
 }
 
 impl<T> AtomicBox<T> {
@@ -12,9 +20,7 @@ impl<T> AtomicBox<T> {
     pub fn new(data: T) -> AtomicBox<T> {
         let arc = Box::new(Arc::new(data));
         let ptr = AtomicPtr::new(Box::into_raw(arc));
-        return AtomicBox{
-            ptr: ptr,
-        }
+        return AtomicBox { ptr: ptr };
     }
     /// Get an `Arc<T>` pointing to the value currently stored in the box
     pub fn load(&self) -> Arc<T> {
@@ -27,14 +33,14 @@ impl<T> AtomicBox<T> {
         let arc = Box::new(Arc::new(data));
         let ptr = Box::into_raw(arc);
         let old_ptr = self.ptr.swap(ptr, Ordering::AcqRel);
-        unsafe {Box::from_raw(old_ptr)};
+        unsafe { Box::from_raw(old_ptr) };
     }
     /// Swap the current value with `data`, returning a `boxed` `Arc<T>` pointing to the old value
     pub fn swap(&self, data: T) -> Box<Arc<T>> {
         let arc = Box::new(Arc::new(data));
         let ptr = Box::into_raw(arc);
         let old_ptr = self.ptr.swap(ptr, Ordering::AcqRel);
-        return unsafe {Box::from_raw(old_ptr)};
+        return unsafe { Box::from_raw(old_ptr) };
     }
 }
 
@@ -44,7 +50,9 @@ impl<T> Drop for AtomicBox<T> {
     }
 }
 
-impl<T> fmt::Debug for AtomicBox<T> where T: fmt::Debug {
+impl<T> fmt::Debug for AtomicBox<T>
+    where T: fmt::Debug
+{
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         f.debug_struct("AtomicBox")
             .field("ptr", &self.load())
